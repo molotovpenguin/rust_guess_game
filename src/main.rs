78efb9fca5d8f1dee7d54181bfca8d3cc2c5fe8a
jp <molotovpenguin@gmail.com>
2022-01-03 12:34:46 -0600

    program now loops with "Too Big"/"Too Small" messages until correct number is guessed

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,29 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
+        
+    loop {
+        let mut guess = String::new();
 
-    println!("Please input your guess.");
+        println!("Guess the number!");
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please Type A Number");
-
-    println!("The secret number is {}", secret_number);
+        let guess: u32 = guess.trim().parse().expect("Please Type A Number");
     
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                println!("The secret number is {}", secret_number);
+                break;
+            }
+        }
     }
-
-    }
+}
