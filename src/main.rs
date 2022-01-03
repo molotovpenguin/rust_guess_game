7e5a681ae919cc21984a8c2bc284ce8a92b41942
jp <molotovpenguin@gmail.com>
2022-01-03 13:39:23 -0600

    modifed to improve handling of invalid input

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,11 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please Type A Number");
-    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }; 
+        
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
